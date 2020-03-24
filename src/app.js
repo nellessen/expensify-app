e109c3ad606270f740d94b183b74aaa23ebf150e
David Nellessen <david.nellessen@gmail.com>
2020-03-24 14:37:26 +0100

    Removed mock expense data

--- src/app.js
+++ src/app.js
@@ -4,18 +4,12 @@ import { Provider } from 'react-redux';
 import 'react-dates/initialize';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
 console.ignoredYellowBox = ['Warning:'];
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
 const state = store.getState();
 

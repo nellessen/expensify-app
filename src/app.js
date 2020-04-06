89816fce7c02efd12ea1a218bed25f0adf21e1ed
David Nellessen <david.nellessen@gmail.com>
2020-04-06 10:52:08 +0200

    Added private Routes

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import 'react-dates/initialize';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
+import { login, logout } from './actions/auth';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 
 console.ignoredYellowBox = ['Warning:'];
 
@@ -18,9 +19,30 @@ const jsx = (
     <AppRouter />
   </Provider>
 );
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+};
 
 ReactDOM.render('<p>Loading...</p>', document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'));
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log('Login', user);
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
+  } else {
+    console.log('Logout');
+    store.dispatch(logout());
+    renderApp();
+    history.push('/');
+  }
 });
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 
 test('Header component should rendered correctly', () => {
-  const wrapper = shallow(<Header />);
+  const wrapper = shallow(<Header startLogout={() => { }} />);
   expect(wrapper).toMatchSnapshot();
+});
+
+test('Header component should call startLogout on button click', () => {
+  const onClickSpy = jest.fn();
+  const wrapper = shallow(<Header startLogout={onClickSpy} />);
+  wrapper.find('button').simulate('click');
+  expect(onClickSpy).toHaveBeenLastCalledWith();
 });
